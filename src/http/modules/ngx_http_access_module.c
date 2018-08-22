404a455ed7f0052d0c128e55fc45c82134c57d52
tzpx468u <tzpx468u@qq.com>
2018-08-22 04:15:55 -0700

    @tzpx468 > learn ngx_http_access_module

--- src/http/modules/ngx_http_access_module.c
+++ src/http/modules/ngx_http_access_module.c
@@ -9,6 +9,163 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
+typedef struct {
+	in_addr_t mask;
+	in_addr_t addr;
+	ngx_uint_t deny;		/* unsigned deny: 1 */
+}ngx_http_access_rule_t;
+
+typedef struct {
+	ngx_array_t *rules;
+}ngx_http_access_loc_conf_t;
+
+static ngx_int_t ngx_http_access_handler(ngx_http_request_t *r);
+
+static char *ngx_http_access_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static void ngx_http_access_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_access_merge_loc_conf(ngx_conf_t *cf, void *prev, void *conf);
+static ngx_int_t ngx_http_access_init(ngx_conf_t *cf);
+
+static ngx_command_t ngx_http_access_commands[] = {
+	{ ngx_string("allow"),
+	  NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF
+	                     | NGX_CONF_TAKE1,
+	  ngx_http_access_rule,
+	  NGX_HTTP_LOC_CONF_OFFSET,
+	  0,
+	  NULL },
+
+	{ ngx_string("deny"),
+	  NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF
+	                     | NGX_CONF_TAKE1,
+	  ngx_http_access_rule,
+	  NGX_HTTP_LOC_CONF_OFFSET,
+	  0,
+	  NULL },
+
+	ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_access_module_ctx = {
+	NULL,
+	ngx_http_access_init,
+
+	NULL,
+	NULL,
+
+	NULL,
+	NULL,
+
+	ngx_http_access_create_loc_conf,
+	ngx_http_access_merge_loc_conf
+};
+
+ngx_module_t ngx_http_access_module = {
+	NGX_MODULE_V1,
+	&ngx_http_access_module_ctx,
+	ngx_http_access_commands,
+	NGX_HTTP_MODULE,
+	NULL,							/* init master */
+	NULL,							/* init module */
+	NULL,							/* init process */
+	NULL,							/* init thread */
+	NULL,							/* exit thread */
+	NULL,							/* exit process */
+	NULL,							/* exit master */
+	NGX_MODULE_V1_PADDING
+};
+
+static char *ngx_http_access_rule(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+	ngx_http_access_loc_conf_t *alcf = conf;
+	ngx_int_t rc;
+	ngx_uint_t all;
+	ngx_str_t *value;
+	ngx_cidr_t cidr;
+	ngx_http_access_rule_t *rule;
+
+	all = 0;
+	ngx_memzero(&cidr, sizeof(ngx_cidr_t));
+
+	value = cf->args->elts;
+
+	if(value[1].len == 3 && ngx_strcmp(value[1].data, "all") == 0){
+		all = 1;
+	} else {
+		rc = ngx_ptocidr(&value[1], cidr);
+		if(rc == NGX_ERROR) {
+			ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "invalid parameter \"%V\"", &value[1]);
+			return NGX_CONF_ERROR;
+		}
+		if(rc == NGX_DONE) {
+			ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "low address bits of %V are meaningless in " &value[1]);
+		}
+	}
+
+	if(cidr.family == AF_INET || all) {
+		if(alcf->rules == NULL) {
+			alcf->rules = ngx_array_create(cf->pool, 4, sizeof(ngx_http_access_rule_t));
+			if(alcf->rules == NULL) {
+				return NGX_CONF_ERROR;
+			}
+		}
+		rule = ngx_array_push(alcf->rules);
+		if(rule == NULL) {
+			return NGX_CONF_ERROR;
+		}
+		rule->mask = cidr.u.in.mask;
+		rule->addr = cidr.u.in.addr;
+		rule->deny = (value[0].data[0] == 'd') ? 1 : 0;
+	}
+
+	return NGX_CONF_OK;
+	
+}
+
+
+static void ngx_http_access_create_loc_conf(ngx_conf_t *cf)
+{
+	ngx_http_access_loc_conf_t *conf;
+	conf = ngx_palloc(cf->pool, sizeof(ngx_http_access_loc_conf_t));
+
+ 	if(conf == NULL) {
+		return NULL;
+	}
+	
+	return conf;
+}
+
+static char *ngx_http_access_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+	ngx_http_access_loc_conf_t *prev = parent;
+	ngx_http_access_loc_conf_t *conf = child;
+
+	if(conf->rules == NULL) {
+		conf->rules = prev->rules;
+	}
+	return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_http_access_init(ngx_conf_t *cf)
+{
+	ngx_http_handler_pt *h;
+	ngx_http_core_main_conf_t *cmcf;
+
+	cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_access_module);
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+
+	if(h == NULL) {
+		return NGX_ERROR;
+	}
+
+	h = ngx_http_access_handler;
+	return NGX_OK;
+}
+
+
+
+/*===============================================================================*/
+#if 0 /* study */
 
 typedef struct {
     in_addr_t         mask;
@@ -461,3 +618,7 @@ ngx_http_access_init(ngx_conf_t *cf)
 
     return NGX_OK;
 }
+
+
+#endif /* study */
+
